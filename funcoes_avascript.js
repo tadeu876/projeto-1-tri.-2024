c264054b17a26571f2d63900d262274c06d735e0
tadeu876 <105741481+tadeu876@users.noreply.github.com>
2024-04-17 14:01:20 -0300

    Create funcoes_avascript.js

---
+++ funcoes_avascript.js
@@ -0,0 +1,10 @@
+function  somar(num1, num2){
+  return num1+num2;
+
+function subtrair(num1, num2)
+  return num1-num2;
+
+function dividir(num1, num2)
+  return num1/num2;
+  function multiplicar(num1, num2)
+  return num1*num2;
